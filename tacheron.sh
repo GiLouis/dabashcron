a1f45da51977f1552cf15aec36495381c737985f
GiLouis <glouis2221@s32.mygb.eu>
2013-05-10 16:35:54 +0200

    Modifications relatives à l'accès des utilisateurs

--- tacheron.sh
+++ tacheron.sh
@@ -8,18 +8,23 @@ function checkIfAllowed {
 	else
 		if [ -s /etc/tacheron.allow ];then
 			if grep --quiet ^$1$ /etc/tacheron.allow;then
+#				echo "$USER présent allow"
 				return 0
 			else
+#				echo "$USER pas présent dans allow"
 				return 1
 			fi
 		else
 			if [ -s /etc/tacheron.deny ];then
 				if grep --quiet ^$1$ /etc/tacheron.deny;then
+#					echo "$USER présent dans deny"
 					return 1
 				else
+#					echo "$USER pas présent dans deny"
 					return 0
 				fi
 			else
+#				echo "$USER absent de allow et deny"
 				return 0
 			fi
 		fi
@@ -30,28 +35,29 @@ echo "TacheronTab v0.01"
 echo "Louis & Léo"
 echo "--------------"
 
-
-echo $(checkIfAllowed "louis")
-
 if [ $EUID -ne 0 ];then
-	if ! checkIfAllowed $USER;then
-		echo "$USER n'es pas autorisé. Faites ajouter votre nom dans /etc/tacheron.allow ou faites-le retirer de /etc/tacheron.deny"
-	else
-		echo "$USER autorisé"
+
+	if [ ! -f /etc/tacherontab ];then
+		echo "Création duf fichier /etc/tacherontab"
+		touch /etc/tacherontab
 	fi
+
+	if [ ! -f /etc/tacheron.allow ];then
+		echo "Nan mais allow quoi ! Création du fichier /etc/tacheron.allow"
+		touch /etc/tacheron.allow
+	fi
+
+	if [ ! -f /etc/tacheron.deny ];then
+		echo "Création du fichier /etc/tacheron.deny"
+		touch /etc/tacheron.deny
+	fi
+
+fi
+
+
+if ! checkIfAllowed $USER &&[ $EUID -ne 0 ];then
+        echo "$USER n'es pas autorisé. Faites ajouter votre nom dans /etc/tacheron.allow ou retirer de /etc/tacheron.deny"
 else
-if [ ! -f /etc/tacherontab ];then
-	echo "Création duf fichier /etc/tacherontab"
-	touch /etc/tacherontab
+        echo "$USER autorisé"
 fi
 
-if [ ! -f /etc/tacheron.allow ];then
-	echo "Nan mais allow quoi ! Création du fichier /etc/tacheron.allow"
-	touch /etc/tacheron.allow
-fi
-if [ ! -f /etc/tacheron.deny ];then
-	echo "Création du fichier /etc/tacheron.deny"
-	touch /etc/tacheron.deny
-fi
-
-fi
